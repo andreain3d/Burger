f51499cff937efcb7b27b024da2dbc29c6809847
Andrea N <andreain3d@gmail.com>
2019-08-11 21:39:09 -0400

    added SQL for database/table setup, set up mysql connection

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Don't You Four Cheddar 'Bout Me Burger", FALSE),
+("Paranormal Pepper Jack-tivity Burger", FALSE),
+("Beets of Burden Burger", FALSE);
