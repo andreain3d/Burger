46595b747c579c226f136d3a267fd6ba68e2bff3
Andrea N <andreain3d@gmail.com>
2019-08-11 22:49:14 -0400

    basic functionality for index to GET and display burgers from db

---
+++ controllers/controller.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var burger = require("../models/burgers.js");
+
+var router = express.Router();
+
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+module.exports = router;
